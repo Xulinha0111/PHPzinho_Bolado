f721f9a333dab99298e8292fc5d75d62bb7ae6cf
Julia B. Sorrente <107429762+Xulinha0111@users.noreply.github.com>
2023-04-05 08:44:22 -0300

    Add files via upload

---
+++ xulinha/Controle Bancário/conta_corrente_exibir.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exibir Conta Corrente</title>
+</head>
+<body>
+    <?php
+        echo '<h3>CONTROLE BANCÁRIO - CADASTRO DE CONTA CORRENTE</H3>';
+    ?>
+    
+</body>
+</html>
