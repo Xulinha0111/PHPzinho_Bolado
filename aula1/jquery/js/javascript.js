731b72c1aee6ff15fb9679829055cc621eb200e7
Julia B. Sorrente <107429762+Xulinha0111@users.noreply.github.com>
2024-08-19 10:44:28 -0300

    Delete aula1/jquery directory

--- aula1/jquery/js/javascript.js
+++
@@ -1,7 +0,0 @@
-//window.onload = iniciar; //Busca a função iniciar, e executa o bloco de códigos que está entre as {}
-
-$(document).ready(iniciar);
-
-function iniciar() {
-    alert("Primeiro script utilizando jQuery")
-}
