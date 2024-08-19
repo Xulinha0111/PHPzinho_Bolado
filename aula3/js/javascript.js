3fdff2b3c81113be8de7e491d96a82332263d3df
Julia B. Sorrente <107429762+Xulinha0111@users.noreply.github.com>
2024-08-19 10:45:36 -0300

    Add files via upload

---
+++ aula3/js/javascript.js
@@ -0,0 +1,6 @@
+$(document).ready(iniciar)
+
+function iniciar() {
+    $("*").css("color", "blue"); // "*" --> setor global, deixa todos os elementos do site com a mesma cor, nesse caso, azul
+    $("p1").css("border", "1px solid")
+}
